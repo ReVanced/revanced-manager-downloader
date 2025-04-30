562e4ceb535805d3b4a0aef6f22cb0fb6d3aad45
oSumAtrIX <johan.melkonyan1@web.de>
2025-04-30 09:19:45 +0200

    refactor: Simplify URL builder

--- src/main/kotlin/app/revanced/manager/plugin/downloader/apkmirror/APKMirrorDownloader.kt
+++ src/main/kotlin/app/revanced/manager/plugin/downloader/apkmirror/APKMirrorDownloader.kt
@@ -6,18 +6,12 @@ import android.net.Uri
 import app.revanced.manager.plugin.downloader.webview.WebViewDownloader
 
 val apkMirrorDownloader = WebViewDownloader { packageName, version ->
-    with(Uri.Builder()) {
-        scheme("https")
-        authority("www.apkmirror.com")
-        mapOf(
-            "post_type" to "app_release",
-            "searchtype" to "apk",
-            "s" to (version?.let { "$packageName $it" } ?: packageName),
-            "bundles%5B%5D" to "apk_files" // bundles[]
-        ).forEach { (key, value) ->
-            appendQueryParameter(key, value)
-        }
-
-        build().toString()
-    }
-}
+    Uri.Builder()
+        .scheme("https")
+        .authority("www.apkmirror.com")
+        .appendQueryParameter("post_type", "app_release")
+        .appendQueryParameter("searchtype", "apk")
+        .appendQueryParameter("s", version?.let { "$packageName $it" } ?: packageName)
+        .appendQueryParameter("bundles%5B%5D" /* bundles[] */, "apk_files")
+        .toString()
+}   
